069929ac595281b1b9176f25f85b5e7cf09f5bd0
Ishan4u <mohamedishan44@gmail.com>
2023-09-07 15:19:56 +0530

    Add Comment plugin

--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -74,6 +74,11 @@ end
 time([[try_loadstring definition]], false)
 time([[Defining packer_plugins]], true)
 _G.packer_plugins = {
+  ["Comment.nvim"] = {
+    loaded = true,
+    path = "/home/linux/.local/share/nvim/site/pack/packer/start/Comment.nvim",
+    url = "https://github.com/numToStr/Comment.nvim"
+  },
   ["packer.nvim"] = {
     loaded = true,
     path = "/home/linux/.local/share/nvim/site/pack/packer/start/packer.nvim",
